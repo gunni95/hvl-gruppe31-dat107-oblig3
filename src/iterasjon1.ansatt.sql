c8073ab9cbdec7247b6f07d547219973fe084267
h669826 <669826@stud.hvl.no>
2023-04-10 13:30:51 +0200

    ENDRET BRUKERNAVN TIL PRIMARY KEY I SQL

--- src/iterasjon1.ansatt.sql
+++ src/iterasjon1.ansatt.sql
@@ -5,7 +5,7 @@ SET search_path TO ansatte;
 
 CREATE TABLE ansatt(
 	Id integer PRIMARY KEY,
-	Brukernavn char(6) NOT NULL,
+	Brukernavn char(6) PRIMARY KEY,
 	Fornavn char(20) NOT NULL,
 	Etternavn char(20) NOT NULL,
 	AnsettelsesDato char(20) NOT NULL,
