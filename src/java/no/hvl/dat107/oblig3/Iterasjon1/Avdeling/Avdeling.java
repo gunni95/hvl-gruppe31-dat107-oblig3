8f59a78b2d8b2e2db61fec0aad38633448223383
GraffN <jorgen.graff-n@outlook.com>
2023-04-11 15:35:51 +0200

    lagt til funksjoner for å lage avdelinger

--- src/java/no/hvl/dat107/oblig3/Iterasjon1/Avdeling/Avdeling.java
+++ src/java/no/hvl/dat107/oblig3/Iterasjon1/Avdeling/Avdeling.java
@@ -1,10 +1,19 @@
 package no.hvl.dat107.oblig3.Iterasjon1.Avdeling;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(schema = "avdelinger")
+@NamedQuery(name = "hentAlleAvdelinger", query = "SELECT a FROM Avdeling as a order by a.avdelingId")
 public class Avdeling {
-    static Integer avdelingId;
+    @Id static Integer avdelingId;
     static String avdelingNavn;
     static String avdelingSjef; // required og burde være en AnsattId
+
 
 
     public Avdeling(Integer avdelingId, String avdelingNavn, String avdelingSjef){
@@ -12,10 +21,23 @@ public class Avdeling {
         this.avdelingNavn = avdelingNavn;
         this.avdelingSjef = avdelingSjef;
     }
+
+    public Avdeling() {
+
+    }
+
     public Integer getAvdelingId(){return avdelingId;}
     public Integer setAvdelingId(Integer avdelingId){this.avdelingId = avdelingId;}
     public String getAvdelingNavn(){return avdelingNavn;}
     public void setAvdelingNavn(String avdelingNavn){this.avdelingNavn = avdelingNavn;}
     public String getAvdelingSjef(){return avdelingSjef;}
     public String setAvdelingSjef(String avdelingSjef){this.avdelingSjef = avdelingSjef;}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
--- src/java/no/hvl/dat107/oblig3/Iterasjon1/Avdeling/AvdelingDAO.java
+++ src/java/no/hvl/dat107/oblig3/Iterasjon1/Avdeling/AvdelingDAO.java
@@ -1,7 +1,7 @@
 package no.hvl.dat107.oblig3.Iterasjon1.Avdeling;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import no.hvl.dat107.oblig3.Iterasjon1.Ansatt.Ansatt;
 
 import java.util.List;
 
@@ -13,31 +13,101 @@ public class AvdelingDAO implements AvdelingDAOInterface {
 
     @Override
     public void lagreAvdeling(Avdeling a) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+        try {
+            tx.begin();
+            em.persist(a); //Oppretter en ny rad i databasen
+            tx.commit();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public Avdeling finnAvdelingMedId(int id) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.find(Avdeling.class, id ); //Henter ut på primærnøkkel
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public Avdeling finnAvdelingMedNavn(String avdelingNavn) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            return em.find(Avdeling.class, avdelingNavn); //Henter ut på primærnøkkel
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public List<Avdeling> hentAlleAvdelinger() {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        String jpqlQuery = "SELECT a FROM Avdeling as a order by a.avdelingId";
+
+        try {
+            TypedQuery<Avdeling> query = em.createQuery(jpqlQuery, Avdeling.class);
+            return query.getResultList(); //Henter ut basert på spørring
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void oppdaterAvdeling(int id, String nyttAvdelingNavn, String nySjef) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+        try {
+            tx.begin();
+
+            Avdeling a = em.find(Avdeling.class, id); //Finne rad som skal oppdateres
+            if(nyttAvdelingNavn != null) {
+                a.setAvdelingNavn(nyttAvdelingNavn);
+            }
+            if(nySjef != null) {
+                a.setAvdelingSjef(nySjef);
+            }
+
+            tx.commit();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void slettAvdeling(int id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+        try {
+            tx.begin();
+
+            Avdeling a = em.find(Avdeling.class, id); //Finne rad som skal slettes
+            em.remove(a); //Slette rad som tilsvarer managed oject a
+
+            tx.commit();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
     }
 }
