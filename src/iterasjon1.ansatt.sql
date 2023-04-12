47d69b57adfddffb44b307b0a741f4ff2698c582
h669826 <669826@stud.hvl.no>
2023-04-12 13:17:31 +0200

    missig settup

--- src/iterasjon1.ansatt.sql
+++ src/iterasjon1.ansatt.sql
@@ -4,17 +4,18 @@ CREATE SCHEMA ansatte;
 SET search_path TO ansatte;
 
 CREATE TABLE ansatt(
-	Id integer PRIMARY KEY,
-	Brukernavn char(6) PRIMARY KEY,
-	Fornavn char(20) NOT NULL,
-	Etternavn char(20) NOT NULL,
-	AnsettelsesDato char(20) NOT NULL,
-	Stilling char(30),
-	MaanedsLonn integer
+                       Id SERIAL,
+                       Brukernavn char(6),
+                       Fornavn char(20) NOT NULL,
+                       Etternavn char(20) NOT NULL,
+                       AnsettelsesDato char(20) NOT NULL,
+                       Stilling char(30),
+                       MaanedsLonn integer,
+                       primary key (Id, Brukernavn)
 );
 
-INSERT INTO ansatt(Id, Brukernavn, Fornavn, Etternavn, AnsettelsesDato, Stilling, MaanedsLonn)
+INSERT INTO ansatt(Brukernavn, Fornavn, Etternavn, AnsettelsesDato, Stilling, MaanedsLonn)
 VALUES
-	(0001, 'Don', 'Don', 'Duck', '2022-01-01', 'Kokk', 40000),
-	(0002, 'Dan', 'Danny', 'Dork', '2022-03-22', 'Murer', 43000),
-	(0003, 'Dam', 'Dam', 'Scheim', '2022-10-12', 'Snikskytter', 48000);
+    ('Don', 'Don', 'Duck', '2022-01-01', 'Kokk', 40000),
+    ('Dan', 'Danny', 'Dork', '2022-03-22', 'Murer', 43000),
+    ('Dam', 'Dam', 'Scheim', '2022-10-12', 'Snikskytter', 48000);
