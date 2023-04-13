0becdd1a5cda1923ebe591a097116df3e54d5bb8
h669826 <669826@stud.hvl.no>
2023-04-13 14:37:27 +0200

    my stuffs

--- src/java/no/hvl/dat107/oblig3/Prosjekt/Prosjektdeltakelse/ProsjektdeltakelseDAO.java
+++ src/java/no/hvl/dat107/oblig3/Prosjekt/Prosjektdeltakelse/ProsjektdeltakelseDAO.java
@@ -1,7 +1,29 @@
 package no.hvl.dat107.oblig3.Prosjekt.Prosjektdeltakelse;
 
-public class ProsjektdeltakelseDAO {
-    public void oppdaterProsjektdeltakelseRolle(String nyRolle){
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
+public class ProsjektdeltakelseDAO {
+    private final EntityManagerFactory emf;
+    public ProsjektdeltakelseDAO() {
+        emf = Persistence.createEntityManagerFactory("ansattPersistenceUnit");
+    }
+    public void oppdaterProsjektdeltakelseRolle(int ansattid, int prosjetkid, String nyRolle){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+            tx.begin();
+            Prosjektdeltakelse l = em.find(Prosjektdeltakelse.class, ansattid);
+            //unfin
+            tx.commit();
+        } catch(Throwable e){
+            e.printStackTrace();
+            tx.rollback();
+        } finally{
+            em.close();
+        }
     }
 }
--- src/java/no/hvl/dat107/oblig3/Prosjekt/Prosjektdeltakelse/ProsjektdeltakelseDAOInterface.java
+++ src/java/no/hvl/dat107/oblig3/Prosjekt/Prosjektdeltakelse/ProsjektdeltakelseDAOInterface.java
@@ -2,7 +2,7 @@ package no.hvl.dat107.oblig3.Prosjekt.Prosjektdeltakelse;
 
 public interface ProsjektdeltakelseDAOInterface {
     /** Update - Oppdater rolle*/
-    void oppdaterProsjektdeltakelseRolle(String nyRolle);
+    void oppdaterProsjektdeltakelseRolle(int id, String nyRolle);
     /** Update - Før inn timer*/
     void addTimer(int timer);
 }
