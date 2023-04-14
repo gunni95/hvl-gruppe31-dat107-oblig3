cb4bd69f62351eb9613d2f734feea9a653c9a66c
GraffN <jorgen.graff-n@outlook.com>
2023-04-14 18:07:25 +0200

    skrivUtBeskrivelse

--- src/java/no/hvl/dat107/oblig3/Prosjekt/ProsjektTekstgrensesnitt.java
+++ src/java/no/hvl/dat107/oblig3/Prosjekt/ProsjektTekstgrensesnitt.java
@@ -1,7 +1,10 @@
 package no.hvl.dat107.oblig3.Prosjekt;
 
+import no.hvl.dat107.oblig3.Ansatt.AnsattDAO;
 import no.hvl.dat107.oblig3.Ansatt.AnsattDAOInterface;
 import no.hvl.dat107.oblig3.Avdeling.AvdelingDAOInterface;
+import no.hvl.dat107.oblig3.Prosjekt.Prosjektdeltakelse.Prosjektdeltakelse;
+import no.hvl.dat107.oblig3.Prosjekt.Prosjektdeltakelse.ProsjektdeltakelseDAO;
 import no.hvl.dat107.oblig3.Teksgrensesnitt;
 
 import java.util.Scanner;
@@ -15,9 +18,10 @@ public class ProsjektTekstgrensesnitt extends Teksgrensesnitt {
         while (!done) {
             String promptTekst = "Velg operasjon:" +
                     " a) Legg til nytt prosjekt\n " +
-                    " b) Registrere prosjektdeltakelse\n " +
-                    " c) Føre timer\n " +
-                    " d) Skrive ut prosjektinformasjon\n" +
+                    " b) oppdater prosjekt\n" +
+                    " c) Registrere prosjektdeltakelse\n " +
+                    " d) Føre timer\n " +
+                    " e) Skrive ut prosjektinformasjon\n" +
                     " 0) Tilbake";
 
             System.out.println(promptTekst);
@@ -31,18 +35,27 @@ public class ProsjektTekstgrensesnitt extends Teksgrensesnitt {
                 case "a": // a) Legg til nytt prosjekt
                     ProsjektTekstgrensesnitt.lagreProsjekt(prDAO);
                     System.out.println("Ny avdeling lagt til.");
+                    break;
+                case "b": // b) Skrive ut prosjektinformasjon
+                    ProsjektTekstgrensesnitt.oppdaterProsjekt(prDAO);
+                    System.out.println("Prosjekt oppdatert");
+                    break;
+
+                case "c": // b) Registrere prosjektdeltakelse
+                    ProsjektTekstgrensesnitt.leggTilDeltaker(prDAO);
+                    System.out.println("Ny prosjekt deltaker lagt til.");
+                    break;
+                case "d": // c) Føre timer
+                    ProsjektTekstgrensesnitt.leggTilTimer(prDAO);
+                    System.out.println("Nye timer lagt til.");
 
                     break;
-                case "b": // b) Registrere prosjektdeltakelse
-
-
-                    break;
-                case "c": // c) Føre timer
+                case "e": // e) Skriv ut prosjekt informasjon
+                    ProsjektTekstgrensesnitt.skrivUtBeskrivelse(prDAO);
+                    System.out.println("Nye timer lagt til.");
 
                     break;
-                case "d": // b) Skrive ut prosjektinformasjon
 
-                    break;
             }
         }
     }
@@ -95,5 +108,149 @@ public class ProsjektTekstgrensesnitt extends Teksgrensesnitt {
         Prosjekt nyttProsjekt = new Prosjekt(prosjekt,beskrivelse);
         DAO.lagreProsjekt(nyttProsjekt);
         return nyttProsjekt;
+    }
+    public static void leggTilDeltaker(ProsjektDAOInterface DAO){
+
+        ProsjektDAO prDAO = new ProsjektDAO();
+        AnsattDAO anDAO = new AnsattDAO();
+        ProsjektdeltakelseDAO adDAO = new ProsjektdeltakelseDAO();
+        Integer prosjektId;
+        Integer ansattId;
+        String rolle;
+
+        Scanner input = new Scanner(System.in);
+
+        prosjektId = safeRead(() -> {
+            System.out.println("Skriv inn prosjek");
+            String res = input.nextLine();
+            return prDAO.finnProsjektMedNavn(res).getId();
+        }, "Ikke gyldig prosjekt");
+
+        ansattId = safeRead(() -> {
+            System.out.println("Skriv inn navn på deltaker:");
+            String res = input.nextLine();
+            return anDAO.finnAnsattMedBrukernavn(res).getId();
+        }, "Ikke gyldig navn");
+
+        rolle = safeRead(() -> {
+            System.out.println("Skriv inn deltaker sin rolle");
+            String prosjektBeskrivelse = input.nextLine();
+
+            return prosjektBeskrivelse;
+        }, "Ikke gyldig rolle");
+
+        Prosjektdeltakelse leggTilDeltaker = new Prosjektdeltakelse(prosjektId, ansattId, rolle);
+        adDAO.opprettProsjektdeltakelse(leggTilDeltaker);
+    }
+    public static void leggTilTimer(ProsjektDAOInterface DAO){
+
+        ProsjektDAO prDAO = new ProsjektDAO();
+        ProsjektdeltakelseDAO pdDAO = new ProsjektdeltakelseDAO();
+        AnsattDAO anDAO = new AnsattDAO();
+        ProsjektdeltakelseDAO adDAO = new ProsjektdeltakelseDAO();
+        Integer prosjektId;
+        Integer ansattId;
+        Integer timer;
+
+        Scanner input = new Scanner(System.in);
+
+        prosjektId = safeRead(() -> {
+            System.out.println("Skriv inn prosjek");
+            String res = input.nextLine();
+            return prDAO.finnProsjektMedNavn(res).getId();
+        }, "Ikke gyldig prosjekt");
+
+        ansattId = safeRead(() -> {
+            System.out.println("Skriv inn navn på deltaker:");
+            String res = input.nextLine();
+            return anDAO.finnAnsattMedBrukernavn(res).getId();
+        }, "Ikke gyldig navn");
+
+        timer = safeRead(() -> {
+            System.out.println("Skriv inn antall nye timer");
+            Integer antallTimer = Integer.parseInt(input.nextLine());
+
+            return antallTimer;
+        }, "Ikke gyldig time Verdi");
+
+        pdDAO.leggTilTimer(prosjektId, ansattId, timer);
+    }
+    public static void oppdaterProsjekt(ProsjektDAOInterface DAO){
+
+        ProsjektDAO prDAO = new ProsjektDAO();
+        ProsjektdeltakelseDAO pdDAO = new ProsjektdeltakelseDAO();
+        AnsattDAO anDAO = new AnsattDAO();
+        Integer prosjektId;
+        String prosjektNavn;
+        String nySjef;
+        String beskrivelse;
+
+        Scanner input = new Scanner(System.in);
+
+        prosjektId = safeRead(() -> {
+            System.out.print("Prosjekt til redigering: ");
+            String res = input.nextLine();
+            if (res.matches("^\\d+$")) {
+                Prosjekt funnet =  prDAO.finnProsjektMedId(Integer.parseInt(res));
+                if (funnet != null) {
+                    return funnet.getId();
+                }
+            }
+            return prDAO.finnProsjektMedNavn(res).getId();
+        }, "Ikke gyldig prosjekt");
+
+        prosjektNavn = safeRead(() -> {
+            System.out.print("Prosjekt navn: ");
+            String res = input.nextLine();
+            if (res.length() == 0) {
+                return null;
+            }
+
+            return res;
+        }, "Ikke gyldig navn");
+
+        nySjef = safeRead(() -> {
+            System.out.print("Ansatt brukernavn: ");
+            String res = input.nextLine();
+            if (res.length() == 0) {
+                return null;
+            }
+
+            String b =  anDAO.finnAnsattMedBrukernavn(res).getBrukernavn();
+
+            if (b == null) {
+                throw new Exception("Ingen ansatt med dette brukernavnet");
+            }
+
+            return b;
+        }, "Ikke gyldig ansatt");
+
+        beskrivelse = safeRead(() -> {
+            System.out.println("Skriv inn prosjekt beskrivelse");
+            String prosjektBeskrivelse = (input.nextLine());
+
+            if(prosjektBeskrivelse.length() == 0) {
+                return null;
+            }
+
+            return prosjektBeskrivelse;
+        }, "Ikke gyldig time Verdi");
+
+        prDAO.oppdaterProsjekt(prosjektId, prosjektNavn, nySjef, beskrivelse);
+    }
+    public static Prosjekt skrivUtBeskrivelse(ProsjektDAOInterface DAO){
+
+        String prosjekt;
+
+        Scanner input = new Scanner(System.in);
+
+        prosjekt = safeRead(() -> {
+            System.out.println("Skriv inn prosjekt navn:");
+            String res = input.nextLine();
+            if(DAO.finnProsjektMedNavn(res) == null){
+                throw new Exception("prosjekte eksisterer ikke");
+            }
+            return DAO.finnProsjektMedNavn(res).getProsjektInfo;
+        }, "Ikke gyldig navn");
     }
 }
